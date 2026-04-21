d2d184b28b62dc5ee99d6b59e61e9aa89e2ac160
Markus Klug <warmack.mk@gmail.com>
2026-04-21 17:00:59 +0200

    Working on integrating the database into a statistics table. Couldn't continue as I managed to break my website.

---
+++ template_files_website/Statistics/template.php
@@ -0,0 +1,9 @@
+<?php
+  session_name("Website");
+  session_start();
+  $host = "localhost";
+  $user = "marklu18";
+  $pwd = "mBqcEULLHw";
+  $db = "marklu18_project";
+  $mysqli=new mysqli($host, $user, $pwd, $db);
+?>
